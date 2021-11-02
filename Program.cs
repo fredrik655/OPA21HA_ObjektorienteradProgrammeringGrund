b122277d1c61552eb216352d4a8843e630656b62
Fredrik655 <26602147+fredrik655@users.noreply.github.com>
2021-11-02 14:01:20 +0100

    Made a project
    Made a base project with vscode/dotnet v5.5.400

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OPA21HA_ObjektorienteradProgrammeringGrund
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
